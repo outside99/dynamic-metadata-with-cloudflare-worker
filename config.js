6234c4b5ed04ba77c332eb6ff6986ed8500b17f6
outside99 <111586267+outside99@users.noreply.github.com>
2025-06-14 19:11:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const config = {
   domainSource: "https://www.asymmetrixintelligence.com/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/article/[^/]+",
+          pattern: "/article/\\d+/?",
           metaDataEndpoint: "https://xdil-abvj-o7rq.e2.xano.io/api:Z3F6JUiu/public_content_article_matadate/{content_id}"
       }
       // Add more patterns and their metadata endpoints as needed
